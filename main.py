e16c4e687ff5e18a1bd26fef4dfed54c68e30fce
ELCHE16 <hcristian1611@gmail.com>
2021-12-11 13:50:10 -0300

    integracion de geolocalizacion y correcciones de tipado

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import csv
 import time
+from geopy.geocoders import SERVICE_TO_GEOCODER, Nominatim
 
 '''
 - Integracion de yolo con opencv 
@@ -32,7 +33,7 @@ def load_yolo()->tuple:
 
 def load_image(img_path: str)->tuple:
     '''
-    - Procesa imagen, se re configura su dimension y se devuelve informacion en forma de tupla
+    - Procesa imagen, se re-configura su dimension y se devuelve informacion en forma de tupla
     '''
 
     img = cv2.imread(img_path)
@@ -187,8 +188,6 @@ def is_yellow(img: list)->bool:
     else:
         return False
 
-# ---------- Fin de funciones para detectar colores -------
-
 
 def get_color(img_path: str)->str:
     '''
@@ -207,6 +206,9 @@ def get_color(img_path: str)->str:
         return "amarillo"
     elif(is_black(img)):
         return "negro"
+
+
+# ---------- Fin de funciones para detectar colores -------
 
 
 def actualizar_stock(productos: dict, color: str, clasificacion: str)->None:
@@ -390,7 +392,7 @@ def validar_str(opcion)->str:
     return ingrese_str
 
 
-def validar_codigo_producto() -> int:
+def validar_codigo_producto()->int:
     codigo: str = input('Ingrese el codigo de producto: ')
     codigos: list = ['1334', '568']
 
@@ -400,7 +402,7 @@ def validar_codigo_producto() -> int:
     return int(codigo)
 
 
-def validar_color_producto(cod_producto) -> str:
+def validar_color_producto(cod_producto)->str:
     color: str = input('Ingrese el color del producto: ')
     colores: dict = {1334: ['rojo', 'azul', 'verde', 'negro', 'amarillo'],
                      568: ['negro', 'azul']}
@@ -411,7 +413,7 @@ def validar_color_producto(cod_producto) -> str:
     return color.lower()
 
 
-def mostrar_stock(stock: dict, cod_articulo: str) -> None:
+def mostrar_stock(stock: dict, cod_articulo: str)->None:
     limpiar()
     if cod_articulo == 1334:
         print(f"""\n\n      STOCK BOTELLAS      
@@ -428,7 +430,7 @@ def mostrar_stock(stock: dict, cod_articulo: str) -> None:
   NEGRO: {stock[568]['color']['negro']}""")
 
 
-def validar_color_stock(stock: dict, cod_articulo: str) -> tuple:
+def validar_color_stock(stock: dict, cod_articulo: str)->tuple:
     color: str = validar_color_producto(cod_articulo)
     cantidad: int = validar_entero(1, 100, 'cantidaad')
 
@@ -442,11 +444,11 @@ def validar_color_stock(stock: dict, cod_articulo: str) -> tuple:
     return color, cantidad
 
 
-def ingresar_producto_a_pedido(stock, n_pedido, fecha, cliente, ciudad, provincia)-> tuple:
+def ingresar_producto_a_pedido(stock, n_pedido, fecha, cliente, ciudad, provincia)->tuple:
     pedido: list = []
     codigo_articulo: int = validar_codigo_producto()
-    color, cantidad = validar_color_stock(stock,codigo_articulo)
-    descuento: int = validar_entero(0,100,'descuento')
+    color, cantidad = validar_color_stock(stock, codigo_articulo)
+    descuento: int = validar_entero(0, 100, 'descuento')
     pedido: list = [n_pedido, fecha, cliente, ciudad, provincia, codigo_articulo, color, cantidad, descuento]
     stock[codigo_articulo]['color'][color] -= cantidad
 
@@ -490,7 +492,7 @@ def ingresar_pedido(stock, estado_pedidos: dict)->tuple:
     return stock, estado_pedidos
 
 
-def remover_pedido_validado(n_pedido: int, estado_pedidos: dict) -> dict:
+def remover_pedido_validado(n_pedido: int, estado_pedidos: dict)->dict:
     lista_actualizada: list = []
     for pedido in estado_pedidos['pedidos validados']:
         if pedido[0] != n_pedido:
@@ -501,7 +503,7 @@ def remover_pedido_validado(n_pedido: int, estado_pedidos: dict) -> dict:
     return estado_pedidos
 
 
-def remover_pedido_cancelado(n_pedido: int, estado_pedidos: dict) -> dict:
+def remover_pedido_cancelado(n_pedido: int, estado_pedidos: dict)->dict:
     lista_actualizada: list = []
     for pedido in estado_pedidos['pedidos cancelados']:
         if pedido[0] != n_pedido:
@@ -519,12 +521,14 @@ def rehacer_pedido(stock: dict, estado_pedidos: dict)->tuple:
     print('Añada un producto')
     datos_pedido: list = []
 
-    for pedido in estado_pedidos['pedidos validados']:  # tomo los datos del pedido ya registrados
+    # tomo los datos del pedido ya registrados
+    for pedido in estado_pedidos['pedidos validados']:
         if numero_pedido == pedido[0]:
             datos_pedido = pedido
             estado_pedidos = remover_pedido_validado(numero_pedido, estado_pedidos)
 
-    for articulo in estado_pedidos['pedidos cancelados']:  # quito el pedido de la lista de cancelados
+    # quito el pedido de la lista de cancelados
+    for articulo in estado_pedidos['pedidos cancelados']:
         if numero_pedido == articulo[0]:
             datos_pedido = articulo
             estado_pedidos = remover_pedido_cancelado(numero_pedido, estado_pedidos)
@@ -552,7 +556,7 @@ def rehacer_pedido(stock: dict, estado_pedidos: dict)->tuple:
     return stock, estado_pedidos
 
 
-def numero_pedidos_validados(estado_pedidos: dict):
+def numero_pedidos_validados(estado_pedidos: dict)->list:
     '''
     - Retorna una lista con los numeros de pedidos validados
     '''
@@ -633,6 +637,251 @@ def inicio_menu_pedidos(productos: dict)->dict:
 # ---- Fin de funciones para procesamiento de ABM de pedidos ------------------------------
 
 
+'''
+- Funciones para la geolocalizacion, determinacion de zona y optimizacion de recorrido optimo
+'''
+
+
+def recoleccion_datos_ciudades()->dict:
+    archivo_pedidos = open('pedidos.csv', 'r', encoding='utf-8')
+    leectura = csv.reader(archivo_pedidos, dialect='excel', delimiter=',', quotechar='|')
+    next(leectura)
+    ciudades: dict = {}
+
+    for row in leectura:
+        if(row != []):
+            if(row[0] not in ciudades):
+                ciudades[row[0]] = [row[3], [row[5], row[7]]]
+            else:
+                ciudades[row[0]].append([row[5], row[7]])
+    archivo_pedidos.close()
+
+    return ciudades
+
+
+def ordenar_norte(zona_norte: dict)->list:
+    orden_norte: list = sorted(zona_norte.items(), key=lambda x: x[1][1], reverse = True)
+    lista_sin_coordenadas_norte: list = []
+
+    for ciudad in orden_norte:
+        lista_sin_coordenadas_norte.append(ciudad[0])
+
+    return lista_sin_coordenadas_norte
+
+
+def ordenar_centro(zona_centro: dict)->list:
+    orden_centro: list = sorted(zona_centro.items(), key=lambda x: x[1][1], reverse = True)
+    lista_sin_coordenadas_centro: list = []
+
+    for ciudad in orden_centro:
+        lista_sin_coordenadas_centro.append(ciudad[0])
+
+    return lista_sin_coordenadas_centro
+
+
+def ordenar_sur(zona_sur: dict)->list:
+    orden_sur: list = sorted(zona_sur.items(), key=lambda x: x[1], reverse = True)
+    lista_sin_coordenadas_sur: list= []
+
+    for ciudad in orden_sur:
+        lista_sin_coordenadas_sur.append(ciudad[0])
+
+    return lista_sin_coordenadas_sur
+
+
+def ordenar_camiones_pesos(dict_camiones: dict, dict_pesos: dict)->tuple:
+    orden_camiones: list = sorted(dict_camiones.items(), key=lambda x: x[1], reverse = True)
+    orden_pesos: list = sorted(dict_pesos.items(), key=lambda x: x[1], reverse = True)
+
+    return orden_camiones, orden_pesos
+
+
+def distribucion_zonas(lista_ciudad: list)->tuple:
+    geolocator = Nominatim(user_agent='API')
+    lugar_coordenadas: dict = {}
+    zona_norte: dict = {}
+    zona_sur: dict = {}
+    zona_centro: dict = {}
+    zona_caba: list = []
+
+    for indice in range(1,len(lista_ciudad)):
+        buscar_ciudad = geolocator.geocode(lista_ciudad[indice], country_codes='AR', timeout=15)
+        latitud_ciudad: float = buscar_ciudad.latitude
+        longitud_ciudad: float = buscar_ciudad.longitude
+        lugar_coordenadas[lista_ciudad[indice]] = [latitud_ciudad, longitud_ciudad]
+    for ciudad in lugar_coordenadas.items():
+        if(ciudad[0] == 'CABA'):
+            zona_caba.append(ciudad[0])
+        elif int(ciudad[1][0]) > -35:
+            zona_norte[ciudad[0]] = ciudad[1]
+        elif int(ciudad[1][0]) < -40:
+            zona_sur[ciudad[0]] = ciudad[1]
+        elif -35 >= int(ciudad[1][0]) >= -40:
+            zona_centro[ciudad[0]] = ciudad[1]
+
+    zona_norte = ordenar_norte(zona_norte)
+    zona_centro = ordenar_centro(zona_centro)
+    zona_sur = ordenar_sur(zona_sur)
+
+    return zona_norte, zona_centro, zona_caba, zona_sur
+
+
+def averiguar_peso(zona_norte: list, zona_centro: list, zona_caba: list, zona_sur: list, dict_pedidos: dict )->tuple:
+    codigo_botella: str = '1334'
+    codigo_vaso: str = '568'
+    peso_botella: int = 450
+    peso_vaso: int = 350
+    peso_sur: int = 0
+    peso_norte: int = 0
+    peso_centro: int = 0
+    peso_caba: int = 0
+
+    for ciudad in dict_pedidos.values():
+        if(ciudad[0] in zona_caba):
+            for lista in ciudad:
+                if lista[0] == codigo_botella:
+                    peso_caba += (int(lista[1]) * peso_botella)
+                elif lista[0] == codigo_vaso:
+                    peso_caba += (int(lista[1]) * peso_vaso)
+        elif(ciudad[0] in zona_sur):
+            for lista in ciudad:
+                if lista[0] == codigo_botella:
+                    peso_sur += (int(lista[1]) * peso_botella)
+                elif lista[0] == codigo_vaso:
+                    peso_sur += (int(lista[1]) * peso_vaso)
+        elif(ciudad[0] in zona_norte):
+            for lista in ciudad:
+                if(lista[0] == codigo_botella):
+                    peso_norte += (int(lista[1]) * peso_botella)
+                elif(lista[0] == codigo_vaso):
+                    peso_norte += (int(lista[1]) * peso_vaso)
+        elif(ciudad[0] in zona_centro):
+            for lista in ciudad:
+                if(lista[0] == codigo_botella):
+                    peso_centro += (int(lista[1]) * peso_botella)
+                elif(lista[0] == codigo_vaso):
+                    peso_centro += (int(lista[1]) * peso_vaso)
+
+    peso_centro: float = peso_centro /1000
+    peso_caba: float = peso_caba /1000
+    peso_norte: float = peso_norte /1000
+    peso_sur: float = peso_sur /1000
+
+    return peso_norte, peso_centro, peso_caba, peso_sur
+
+
+
+def hacer_viaje_optimo(dict_pedidos:dict,opcion: int)->list:
+    ciudades: list = []
+    zona_norte_ciudades : list = []
+    zona_centro_ciudades : list = []
+    zona_caba_ciudades : list = []
+    zona_sur_ciudades : list = []
+    for datos in dict_pedidos.values():
+        ciudades.append(datos[0])
+    zona_norte, zona_centro, zona_caba, zona_sur = distribucion_zonas(ciudades)
+    for ciudad_en_zona in zona_norte:
+        zona_norte_ciudades.append(ciudad_en_zona[0])
+    for ciudad_en_zona in zona_centro:
+        zona_centro_ciudades.append(ciudad_en_zona[0])
+    for ciudad_en_zona in zona_sur:
+        zona_sur_ciudades.append(ciudad_en_zona[0])
+    zona_caba_ciudades.append(zona_caba[0])
+    if(opcion == 1):
+        return zona_norte_ciudades
+    elif(opcion == 2):
+        return zona_centro_ciudades
+    elif(opcion == 3):
+        return zona_caba_ciudades
+    elif(opcion == 4):
+        return zona_sur_ciudades
+
+
+def hacer_camiones(dict_pedidos:dict)->list:
+    pedidos_que_salen: list = []
+    zona_norte: dict = {}
+    zona_sur: dict = {}
+    zona_centro: dict = {}
+    zona_caba: list = []
+    peso_zonas: list = {}
+    camiones_disponible: dict = {'utilitero1': 600, 'utilitero2': 1000, 'utilitero3': 500, 'utilitero4': 2000}
+    ciudades: list = []
+
+    for datos in dict_pedidos.values():
+        ciudades.append(datos[0])
+
+    zona_norte, zona_centro, zona_caba, zona_sur = distribucion_zonas(ciudades)
+    peso_norte, peso_centro, peso_caba, peso_sur = averiguar_peso(zona_norte, zona_centro, zona_caba, zona_sur, dict_pedidos)
+    peso_zonas['peso_norte'] = peso_norte
+    peso_zonas['peso_centro'] = peso_centro
+    peso_zonas['peso_caba'] = peso_caba
+    peso_zonas['peso_sur'] = peso_sur
+    camiones_disponible, peso_zonas = ordenar_camiones_pesos(camiones_disponible, peso_zonas)
+    for indice in range(len(camiones_disponible)):
+        if(peso_zonas[indice][1] < camiones_disponible[indice][1]):
+            pedidos_que_salen.append([camiones_disponible[indice][0], peso_zonas[indice][0], peso_zonas[indice][1]])
+    for pedido in pedidos_que_salen:
+        if('norte' in pedido[1]):
+            pedido.append(zona_norte)
+            pedido.append('zona_norte')
+        elif('centro' in pedido[1]):
+            pedido.append(zona_centro)
+            pedido.append('zona_centro')
+        elif('caba' in pedido[1]):
+            pedido.append(zona_caba)
+            pedido.append('zona_caba')
+        elif('sur' in pedido[1]):
+            pedido.append(zona_sur)
+            pedido.append('zona_sur')
+
+    return pedidos_que_salen
+
+
+def escribir_txt(datos:list)->None:
+    if os.path.exists("salida.txt"):
+        os.remove("salida.txt")
+    archivo = open('salida.txt', 'a', encoding='utf-8')
+
+    for pedido in datos:
+        archivo.write('\n')
+        archivo.write(pedido[4])
+        archivo.write('\n')
+        archivo.write(pedido[0])
+        archivo.write('\n')
+        archivo.write(str(pedido[2]))
+        archivo.write('\n')
+        for ciudad in pedido[3]:
+            archivo.write(ciudad)
+            archivo.write('\n')
+
+    archivo.close
+
+
+# --- Fin de funciones de geolocalizacion y recorrido optimo --------
+
+
+# opcion 7)
+def generar_archivos_productos(productos: dict)->None:
+    '''
+    Se crean archivos botellas.txt y vasos.txt
+    '''
+    color_botellas: dict = productos[1334]["color"]
+    color_vasos: dict = productos[568]["color"]
+
+    archivo_botellas = open("botellas.txt", "w")
+    for color in range(len(color_botellas)):
+        nombre_color: str = list(color_botellas.keys())[color]
+        valor_color: str = list(color_botellas.values())[color]
+        archivo_botellas.write(f"{nombre_color.capitalize()}: {valor_color} \n")
+    archivo_botellas.close()
+
+    archivo_vasos = open("vasos.txt", "w")
+    for color in range(len(color_vasos)):
+        nombre_color: str = list(color_vasos.keys())[color]
+        valor_color: str = list(color_vasos.values())[color]
+        archivo_vasos.write(f"{nombre_color.capitalize()}: {valor_color} \n")
+    archivo_vasos.close()
+
 def main()->None:
     diccionario_productos: dict = {1334: {"precio": 15, "peso": 450, "color": {"verde": 0, "rojo": 0, "azul": 0, "negro": 0, "amarillo": 0}},
                                    568: {"precio": 8, "peso": 350, "color": {"azul": 0, "negro": 0}}
@@ -648,6 +897,13 @@ def main()->None:
     while(condicion_menu):
         print("Ingrese una de las siguientes opciones: ")
         print("1- ABM (Alta; Baja; Modificacion) de pedidos.")
+        print("2- Determinar recorrido optimo por zona")
+        print("3- Procesar recorrido y listar recorridos por zona")
+        print("4- Listar pedidos completados por fecha")
+        print("5- Determinar pedidos realizados a Rosario y valorizarlos")
+        print("6- Articulo mas pedido y cual de ellos fue entregado")
+        print("7- Crear archivos de vasos y botellas procesadas por color")
+
         opcion: str = input("Opcion: ")
 
         if(not opcion.isnumeric() or int(opcion) > 7 or int(opcion) < 1):
@@ -655,6 +911,15 @@ def main()->None:
         else:
             if(int(opcion) == 1):
                 estado_pedidos = inicio_menu_pedidos(diccionario_productos)
-
-
-
+            elif(int(opcion) == 2):
+                print(2)
+            elif(int(opcion) == 3):
+                print(3)
+            elif(int(opcion) == 4):
+                print(4)
+            elif(int(opcion) == 5):
+                print(5)
+            elif(int(opcion) == 6):
+                print(6)
+            elif(int(opcion) == 7):
+                generar_archivos_productos(diccionario_productos)
